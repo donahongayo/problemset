27ec3f0fc11c0f95d9710a16d5fcfb5c57f6a2ee
Donah Ongayo <donngeezzzzzzz@gmail.com>
2020-06-17 17:57:38 +0800

    feat(solutions): Solutions to problem sets 1-3

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
 // solutions here
+"use strict";
+
+const asyncOp = require("./lib/lib").asyncOp;
+const EventEmitter = require("events");
+const RandStream = require("./lib/lib").RandStream;
+
+//Problem set 1
+async function doAsync(input) {
+  if (!Array.isArray(input)) {
+    console.log("Not an array");
+  } else {
+    for (const arr of input) {
+      if (Array.isArray(arr)) {
+        arr.map((data) => asyncOp(data));
+        await sleep(1000);
+      } else {
+        await asyncOp(arr);
+      }
+    }
+  }
+}
+async function sleep(millis) {
+  return new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+//Problem set 2
+class RandStringSource extends EventEmitter {
+  constructor(rStream) {
+    super(rStream);
+    this.randStream = rStream;
+    this.genStr = "";
+    this.strEnclosed(this.randStream);
+  }
+
+  strEnclosed(str) {
+    str.on("data", (data) => {
+      this.genStr += data;
+      var count = (
+        this.genStr.match(/[@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g) || []
+      ).length;
+      if (count > 1) {
+        var firstIndex = this.genStr.indexOf(".") + 1;
+        var lastIndex = this.genStr.lastIndexOf(".");
+        this.genStr = this.genStr.substring(firstIndex, lastIndex);
+        console.log(this.genStr);
+        this.emit("data");
+      }
+    });
+  }
+}
+
+//Problem set 3
+class ResourceManager {
+  constructor(cnt) {
+    this.count = cnt;
+    this.resource = 0;
+  }
+
+  borrow(callback) {
+    var ncount = "";
+    if (this.resource < this.count) {
+      ncount += callback(this.resource++);
+    } else {
+      const rsrc = new Resource();
+      rsrc.release = () => {
+        callback(this.resource - this.count);
+      };
+      callback(rsrc);
+    }
+    return ncount;
+  }
+}
+
+class Resource {
+  constructor() {
+    this.resourceCall = null;
+  }
+  release() {
+    this.resourceCall();
+  }
+}
+
+module.exports = {
+  doAsync,
+  RandStringSource,
+  ResourceManager,
+};
