a73e9d070bace8e6b55eb53a346b6ee5682380cd
Donah Ongayo <donngeezzzzzzz@gmail.com>
2020-06-17 18:01:46 +0800

    feat(RandStreamTest): Testing for Randstream Problem Set 2

--- test/rand-stream.js
+++ test/rand-stream.js
@@ -1,10 +1,24 @@
-/* Your tests will go here */
-var RandStream = require('../lib/lib').RandStream,
-  assert = require('assert');
+/* Your tests will go here 
+var RandStream = require("../lib/lib").RandStream,
+  assert = require("assert");
 
-describe('Sample RandStream test', function () {
-  it('should pass if stream is readable', function () {
+describe("Sample RandStream test", function () {
+  it("should pass if stream is readable", function () {
     var stream = new RandStream();
     assert(stream.readable);
+  });
+});
+*/
+const RandStringSource = require("../index").RandStringSource;
+const RandStream = require("../lib/lib").RandStream;
+
+describe("Problem Set 2", function () {
+  this.timeout(3000);
+  it("---", function (done) {
+    var source = new RandStringSource(new RandStream());
+    source = (data) => {
+      console.log(data);
+    };
+    done();
   });
 });
